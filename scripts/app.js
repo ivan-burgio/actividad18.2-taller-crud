72da5eba668b99769f89fec1d72f42d10d0d9766
ivan-burgio <ivan.24.burgio@gmail.com>
2023-11-01 20:49:38 -0300

    Metodo delete y mitad del modificar

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
 let urlA = "https://6542c0fc01b5e279de1f8707.mockapi.io/users";
 
 const btnBuscar = document.getElementById('btnGet1');
-const txtBuscar = document.getElementById('inputGet1Id');
 
+const txtBuscar = document.getElementById('inputGet1Id');
 const btnAgregar = document.getElementById('btnPost');
+
+const btnModificar = document.getElementById('btnPut');
+const txtModificar = document.getElementById('inputPutId');
+
+const btnDelete = document.getElementById('btnDelete');
+const txtDelete = document.getElementById('inputDelete');
 
 let fetchJSONData = function (url, type) {
     let result = {};
@@ -68,6 +74,43 @@ btnAgregar.addEventListener('click', () => {
         name: txtName.value,
         lastname: txtLastName.value,
     }
-    listUsersAdd(NameLastName);
 
+    listUsersAdd(NameLastName);
 });
+
+function listUsersUpdate(id, infoAdd) {
+    let type = { method: "PUT", headers: { "Content-type": "application/json; charset=UTF-8" }, body: JSON.stringify(infoAdd) };
+    fetchJSONData(urlA + '/' + id, type).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            let users = resultObj.data
+            console.log(users);
+        }
+    });
+}
+
+btnModificar.addEventListener('click', () => {
+    const newName  = document.getElementById('inputPutNombre');
+    const newLastName = document.getElementById('inputPutApellido');
+    const newA = {
+        name: newName.value,
+        lastname: newLastName.value,
+    }
+    const infoAdd = newA;
+    const userId = txtModificar.value;
+    console.log(userId)
+    listUsersUpdate(userId, infoAdd);
+})
+
+function listUsersDelete(id) {
+    let type = { method: "DELETE", headers: { "Content-type": "application/json; charset=UTF-8" }};
+    fetchJSONData(urlA + '/' + id, type).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            let users = resultObj.data
+            console.log(users);
+        }
+    });
+}
+
+btnDelete.addEventListener('click', () => {
+    listUsersDelete(txtDelete.value);
+})
